219f90d5e6c19010b6e53eaf7affbf7c8e384977
jojohuang92 <johuang92us@gmail.com>
2025-11-11 23:51:34 -0800

    get clean json file whem start

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ category_file = "categories.json"
 account_file = "accounts.json"
 budget_file = "budgets.json"
 
+for file in [category_file, account_file, budget_file]:
+    if os.path.exists(file):
+        os.remove(file)
+
 if "categories" not in st.session_state:
     if os.path.exists(category_file):
         with open(category_file, "r") as f:
