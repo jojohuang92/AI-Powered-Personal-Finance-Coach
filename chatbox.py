e9a8df3b4f7bad2e5654230a33f784d87bb3cc4c
Jonathan Huang <johuang92us@gmail.com>
2025-11-12 23:40:31 -0800

    gemini chatbox function added

--- chatbox.py
+++ chatbox.py
@@ -1,2 +1,21 @@
-def response():
-    pass
+import streamlit as st
+import os
+import pandas as pd
+from google import genai
+from dotenv import load_dotenv
+
+def response(financial_data: pd.DataFrame, message: str):
+    try:
+        load_dotenv()
+        api = os.getenv("GEMINI_API_KEY")
+        client = genai.Client(api_key=api)
+    except AttributeError:
+        st.error("API Key not found")
+    
+    
+    data_string = financial_data.to_string()
+    
+    prompt = f"You are an expert financial coach. Analyze the following financial data and answer the user's question.\n\nFinancial Data:\n{data_string}\n\nUser's Question:\n{message}"
+    
+    ai_response = client.models.generate_content(model= "gemini-2.5-flash", contents= prompt)
+    return ai_response.text
